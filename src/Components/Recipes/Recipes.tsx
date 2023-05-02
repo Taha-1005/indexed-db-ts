575da19c1fcbc407b88a9411f0d3b44b6ad1bb0a
Taha_Firoz <tahafbharucha@gmail.com>
2023-05-02 11:06:09 +0300

    Posting and deleting

--- src/Components/Recipes/Recipes.tsx
+++ src/Components/Recipes/Recipes.tsx
@@ -1,7 +1,6 @@
 import { MouseEventHandler, useEffect, useState } from 'react';
 import './RecipeStyles.css';
 import Card from '../RecipeCard/Card';
-import { json } from 'stream/consumers';
 
 interface Order {
   OrderId: number;
@@ -18,6 +17,8 @@ function Recipes() {
   const [allOrdersInDb, setAllOrdersInDb] = useState<[] | any>();
   const [recipeOrder, setRecipeOrder] = useState<any>([]);
   const [orderCost, setOrderCost] = useState<number>(0);
+  const [orderDataFetch, setOrderDataFetch] = useState<number>(0);
+  // const [recipeDataFetch, setRecipeDataFetch] = useState<number>(0);
 
   const createRecipeCollectionInIndexedDB = new Promise<string>(
     (resolve, reject) => {
@@ -101,7 +102,6 @@ function Recipes() {
   const getAllRecipes = new Promise<any>((resolve, reject) => {
     // console.log('here in getting all recipes from Indexed DB');
     const dbPromise = idb.open('restaurant-db', 2);
-    let data: any = null;
 
     dbPromise.onsuccess = () => {
       try {
@@ -113,6 +113,9 @@ function Recipes() {
         recipeQuery.onsuccess = () => {
           // console.log(recipeQuery.result);
           // return recipeQuery.result;
+          tx.oncomplete = () => {
+            db.close();
+          };
           resolve(recipeQuery.result);
         };
 
@@ -127,10 +130,6 @@ function Recipes() {
             )
           );
         };
-
-        tx.oncomplete = () => {
-          db.close();
-        };
       } catch (e: any) {
         console.log('Exception occured');
         console.log(e);
@@ -138,7 +137,7 @@ function Recipes() {
     };
   });
 
-  const fetchDataFromApi = new Promise<string>((resolve, reject) => {
+  const fetchDataFromApi = new Promise<string>((resolve) => {
     // console.log('here in fetch');
     fetch('http://168.187.115.103:92/api/FoodProduct', {
       method: 'GET',
@@ -169,8 +168,8 @@ function Recipes() {
     return currentDate;
   }
 
-  function getAllOrders() {
-    console.log('Getting all orders');
+  const getAllOrders = new Promise<any>((resolve, reject) => {
+    // console.log('Getting all orders');
     const dbPromise = idb.open('restaurant-db', 2);
 
     dbPromise.onsuccess = () => {
@@ -179,21 +178,69 @@ function Recipes() {
         const tx = db.transaction('orderDetails', 'readonly');
         const orderStore = tx.objectStore('orderDetails');
 
-        const recipeQuery = orderStore.getAll();
-        recipeQuery.onsuccess = () => {
-          setAllOrdersInDb(recipeQuery.result);
-          console.log(allOrdersInDb);
+        const orderQuery = orderStore.getAll();
+        orderQuery.onsuccess = () => {
+          // setAllOrdersInDb(orderQuery.result);
+          // console.log(allOrdersInDb);
+          // console.log(orderQuery.result);
+          resolve(orderQuery.result);
+          tx.oncomplete = () => {
+            db.close();
+          };
         };
 
-        recipeQuery.onerror = (event) => {
+        orderQuery.onerror = (event) => {
           console.log(
             'Error occured while fetching data from orders Object Store'
           );
           console.log(event);
+          reject(
+            new Error(
+              'Error occured while fetching data from orders Object Store'
+            )
+          );
+        };
+      } catch (e: any) {
+        console.log('Exception occured');
+        console.log(e);
+      }
+    };
+  });
+
+  async function sendOrderToApi(orderData: Order) {
+    console.log(JSON.stringify(orderData));
+    await fetch('http://168.187.115.103:92/api/Order', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(orderData),
+    }).then(async (result) => {
+      const resultInJson = await result.json();
+      console.log(resultInJson);
+    });
+  }
+
+  function deleteOrderFromIndexedDb(orderIndexToDelete: number) {
+    const dbPromise = idb.open('restaurant-db', 2);
+
+    dbPromise.onsuccess = () => {
+      try {
+        const db = dbPromise.result;
+        const tx = db.transaction('orderDetails', 'readwrite');
+        const orderStore = tx.objectStore('orderDetails');
+
+        const orderQuery = orderStore.delete(orderIndexToDelete);
+        orderQuery.onsuccess = () => {
+          tx.oncomplete = () => {
+            db.close();
+          };
+          console.log(orderQuery);
         };
 
-        tx.oncomplete = () => {
-          db.close();
+        orderQuery.onerror = (event) => {
+          console.log(
+            'Error occured while fetching data from orders Object Store'
+          );
+          console.log(event);
         };
       } catch (e: any) {
         console.log('Exception occured');
@@ -201,26 +248,6 @@ function Recipes() {
       }
     };
   }
-
-  async function sendOrderToApi(orderData: Order) {
-    // const toSend = {
-    //   OrderId: orderData.orderId,
-    //   OrderDate: orderData.orderDate,
-    //   ItemId: orderData.itemId,
-    //   ItemName: orderData.itemName,
-    //   ItemAmount: orderData.itemAmount,
-    // };
-
-    await fetch('http://168.187.115.103:92/api/Order', {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.parse(JSON.stringify(orderData)),
-    }).then(async (result) => {
-      const resultInJson = await result.json();
-      console.log(resultInJson);
-    });
-  }
-
   function storeOrderintoIndexedDb(orderData: Order) {
     const dbPromise = idb.open('restaurant-db', 2);
 
@@ -255,47 +282,67 @@ function Recipes() {
     };
   }
 
-  function pushOrderDataToApiOrStoreInDatabase(
+  function handleSynchronizeServerButtonClick(
     event: React.MouseEvent<HTMLButtonElement>
   ) {
     console.log('Sending');
+    allOrdersInDb.map(async (order: Order) => {
+      await sendOrderToApi(order)
+        .then(async () => {
+          deleteOrderFromIndexedDb(order.OrderId);
+          setOrderDataFetch((orderDataFetch: number) => orderDataFetch + 1);
+        })
+        .catch((error) => {
+          console.log('Error is posting order', order);
+          console.log(error);
+        });
+    });
+  }
+
+  function handlePlaceOrderButtonClick(
+    event: React.MouseEvent<HTMLButtonElement>
+  ) {
+    console.log('storing placed order into Indexed DB');
+    setOrderDataFetch(orderDataFetch + 1);
     const recipeIndex = recipeOrder[0];
     const orderToSend: Order = {
-      OrderId: 1,
+      OrderId: Math.floor(Math.random() * 10000 + 1),
       OrderDate: getTodayDate(),
       ItemId: recipeData[recipeIndex].FoodProductId,
       ItemName: recipeData[recipeIndex].FoodProductName,
       ItemAmount: recipeData[recipeIndex].FoodItemsCost,
     };
 
-    sendOrderToApi(orderToSend).catch((error) => {
-      console.log('Error is posting order', orderToSend);
-      console.log(error);
-    });
-
     storeOrderintoIndexedDb(orderToSend);
   }
 
   useEffect(() => {
     const create = async () => {
-      const one = await createRecipeCollectionInIndexedDB;
-      console.log(one);
-      await fetchDataFromApi.then((result) => {
-        storeDataIntoIndexedDb(result);
-      });
-      await getAllRecipes.then((data) => {
-        console.log(data);
-        console.log('setting');
-        setRecipeData(data);
-      });
+      await createRecipeCollectionInIndexedDB;
+      await fetchDataFromApi
+        .then((result) => {
+          storeDataIntoIndexedDb(result);
+        })
+        .then(async () => {
+          await getAllRecipes.then((data) => {
+            console.log(data);
+            console.log('setting');
+            setRecipeData(data);
+          });
+        });
     };
     create().catch(console.error);
   }, []);
 
   useEffect(() => {
-    getAllOrders();
-    console.log(allOrdersInDb);
-  }, [2]);
+    const fetchOrdersInUseEffect = async () => {
+      await getAllOrders.then((result) => {
+        setAllOrdersInDb(result);
+        console.log(allOrdersInDb);
+      });
+    };
+    fetchOrdersInUseEffect().catch(console.error);
+  }, [orderDataFetch]);
 
   function addInRecipeList(recipeIndex: number) {
     // console.log(recipeIndex);
@@ -305,7 +352,7 @@ function Recipes() {
     setOrderCost(orderCost + recipeData[recipeIndex].FoodItemsCost);
   }
 
-  // console.log(recipeData);
+  // console.log(allOrdersInDb);
   return (
     <div className='recipes'>
       <div className='recipe_cards'>
@@ -322,25 +369,34 @@ function Recipes() {
             />
           ))}
       </div>
-      <div className='recipe_order'>
-        {recipeOrder.map((recipeIndex: number, orderIndex: number) => (
-          <div key={orderIndex}>
-            <p>
-              {recipeData[recipeIndex].FoodProductName} -{' '}
-              {recipeData[recipeIndex].FoodItemsCost}
-            </p>
-          </div>
-        ))}
-        <p>Total Cost - {orderCost}</p>
-        <button onClick={pushOrderDataToApiOrStoreInDatabase}>
-          Place the order
-        </button>
-      </div>
       <div>
-        {allOrdersInDb &&
-          allOrdersInDb.map((orderData: Order, orderIndex: number) => {
-            <div key={orderIndex}>{orderData.ItemName}</div>;
-          })}
+        <div className='recipe_order'>
+          {recipeOrder.map((recipeIndex: number, orderIndex: number) => (
+            <div key={orderIndex}>
+              <p>
+                {recipeData[recipeIndex].FoodProductName} -{' '}
+                {recipeData[recipeIndex].FoodItemsCost}
+              </p>
+            </div>
+          ))}
+          <p>Total Cost - {orderCost}</p>
+          <button onClick={handlePlaceOrderButtonClick}>Place the order</button>
+          <br />
+          <br />
+          <button onClick={handleSynchronizeServerButtonClick}>
+            Synchronize with the server
+          </button>
+        </div>
+        <div className='recipe_order_in_db'>
+          These are the orders in the local Data base:
+          {allOrdersInDb &&
+            allOrdersInDb.length > 0 &&
+            allOrdersInDb.map((orderData: any, orderIndex: number) => (
+              <div key={orderIndex}>
+                {orderData.ItemName} {orderIndex}
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
